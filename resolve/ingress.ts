9e8ab93c9300fa4f0d0b3ecbbeb7ed514ba3e31a
harshitha-g2 <harshitha.g2@hp.com>
2020-07-03 03:32:52 +0530

    deploy ingress

--- resolve/ingress.ts
+++ resolve/ingress.ts
@@ -1,10 +1,11 @@
 import { ClusterDiagram } from "../db/cluster-diagram";
-import { V1beta1IngressClass } from "@kubernetes/client-node";
+import { ExtensionsV1beta1Ingress } from "@kubernetes/client-node";
 import Utils from "../lib/utils/utils"
+import { DeployIngress } from "../kube/resolve/ingress"
 
 export class IngressResolvers {
 	public static resolve = async (_: any, args: { [argName: string]: any }) => {
-		let ingress = new V1beta1IngressClass();
+		let ingress = new ExtensionsV1beta1Ingress()
 		var metadata = args.metadata;
 		let diag = new ClusterDiagram("mongodb://localhost:27017")
 
@@ -16,6 +17,7 @@ export class IngressResolvers {
 		ingress.spec = args.spec;
 		ingress.metadata = metadata;
 		await diag.add(args.cluster, ingress)
+		DeployIngress.deployIngress( metadata?.namespace || "default", ingress );
 		return ingress;
 	}
 }
